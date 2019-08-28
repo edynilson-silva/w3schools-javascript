871ed5a5d7f5e16d43297fc66b1258620a90cf19
Edynilson Silva <edynilson.barros@gmail.com>
2019-08-27 22:20:03 -0300

    novos exemplos

---
+++ hide-show-elements/animations.js
@@ -0,0 +1,9 @@
+var paragrafo = document.getElementById('demo');
+
+function hide(){
+    paragrafo.style.display = 'none';
+}
+
+function show(){
+    paragrafo.style.display = 'block';
+}
---
+++ lampada/lampada.js
@@ -0,0 +1,9 @@
+var image = document.getElementById('myImage');
+
+function ligar(){
+    image.src='images/ligada.gif';
+}
+
+function desligar(){
+    image.src='images/desligada.gif';
+}
