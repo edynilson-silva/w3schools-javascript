836285cf257dd08d5ef8d179c61a2a230271d458
Edynilson Silva <edynilson.barros@gmail.com>
2019-08-30 21:09:31 -0300

    Outros tópicos

---
+++ statements/variables.js
@@ -0,0 +1,16 @@
+var a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+
+// Outra maneira de escrever
+// a = 5; b = 6; c = a + b;
+
+
+var person = "John Doe", carName = "Volvo", price = 200;
+
+// Ou...
+
+var person = "John Doe",
+carName = "Volvo",
+price = 200;
