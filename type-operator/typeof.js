ea2f64c26d4eb3804a36137e774f64f7a2e66fd7
Edynilson Silva <edynilson.barros@gmail.com>
2019-08-31 10:47:36 -0300

    operador typeof

---
+++ type-operator/typeof.js
@@ -0,0 +1,9 @@
+typeof ""             // Returns "string"
+typeof "John"         // Returns "string"
+typeof "John Doe"     // Returns "string"
+
+typeof 0              // Returns "number"
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof (3)            // Returns "number"
+typeof (3 + 4)        // Returns "number"
